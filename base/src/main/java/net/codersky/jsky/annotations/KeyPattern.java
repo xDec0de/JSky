91c6d93a0e770998355f4afb9f64d3c8d328fa77
xDec0de_ <xdec0deyt@gmail.com>
2025-04-15 00:37:46 +0200

    Made KeyPattern more strict (See desc)
    This mostly corrects edge cases, like using only dashes as a key, empty string keys, keys that contain more than two consecutive dashes, or end / start with dashes. There are only two formats allowed:
    - nodashes
    - with-dashes
    
    And that's all. This annotation makes it easier to use the key pattern without unexpected errors, as it will warn you about an invalid pattern being used (At least if you are using IntelliJ). By the way, the reason I went with dashes instead of underscores ('_') or dots ('.') is just to make this pattern more "unique". as underscores are already being used by enum names (Upper case, I know) and using dots would conflict with the DataMap class, that uses them to separate between DIFFERENT keys, so combining that with this pattern would look like:
    - parent-key.child-key
    And both the parent key and child key would be respecting the KeyPattern annotation, as they are both INDIVIDUAL keys, even if they are related as parent and child keys.

--- base/src/main/java/net/codersky/jsky/annotations/KeyPattern.java
+++ base/src/main/java/net/codersky/jsky/annotations/KeyPattern.java
@@ -2,6 +2,7 @@ package net.codersky.jsky.annotations;
 
 import net.codersky.jsky.strings.JStrings;
 import org.intellij.lang.annotations.Pattern;
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,17 +13,33 @@ import java.lang.annotation.Target;
  * The KeyPattern annotation claims that any element annotated
  * with it must follow the key format that is commonly used on
  * JSky as a standard. This pattern only allows lowercase
- * characters separated by dashes ('-'). This standard is
- * most commonly used on {@link java.util.Map maps}. The use
- * of this pattern on {@link CharSequence CharSequences} can be verified
- * with {@link JStrings#hasKeyPattern(CharSequence...)}
+ * words separated by single dashes ('-'). This standard is
+ * most commonly used on {@link java.util.Map maps}. The
+ * {@link JStrings#hasKeyPattern(CharSequence)} method is provided
+ * to check if a {@link CharSequence} follows this pattern.
+ * <p>
+ * The pattern enforces the following rules:
+ * <ul>
+ *     <li>Only lowercase letters (a-z) and single dashes are allowed</li>
+ *     <li>Consecutive dashes are not permitted ("hello--world" is invalid)</li>
+ *     <li>Dashes must be between words ("-hello-world-" is invalid)</li>
+ *     <li>Empty strings are not allowed</li>
+ *     <li>Dashes are optional ("helloworld" is valid)</li>
+ * </ul>
+ * <p>
+ * Note: This annotation does not imply {@link NotNull}. For {@code null}
+ * safety, use both annotations together: {@code @NotNull @KeyPattern}
  * <p>
  * This annotation uses the {@link Pattern} annotation with
- * the pattern <i>"[a-z-]+"</i> for static analysis. Sadly, this means
+ * the pattern <i>"[a-z]+(-[a-z]+)*"</i> for static analysis. Sadly, this means
  * that it cannot be combined with other patterns, not until we find
  * a solution that doesn't rely on the {@link Pattern} annotation.
+ *
+ * @since JSky 1.0.0
+ *
+ * @see JStrings#hasKeyPattern(CharSequence)
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
-@Pattern("[a-z-]+")
+@Pattern("[a-z]+(-[a-z]+)*")
 public @interface KeyPattern {}
