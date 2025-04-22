51303b7d6c2711ac442003129c04da87212d286e
xDec0de_ <xdec0deyt@gmail.com>
2025-04-22 18:27:13 +0200

    TestJFiles: Remove tmp files after testing

--- base/src/test/java/net/codersky/jsky/test/TestJFiles.java
+++ base/src/test/java/net/codersky/jsky/test/TestJFiles.java
@@ -10,14 +10,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TestJFiles {
 
 	final File one = new File(JTestUtils.TMP_FOLDER, "one");
-	final File nested = new File(JTestUtils.TMP_FOLDER, "nest/nested");
+	final File nest = new File(JTestUtils.TMP_FOLDER, "nest");
+	final File nested = new File(nest, "nested");
 
 	@Test
 	public void testFileCreation() {
-		JTestUtils.TMP_FOLDER.delete();
 		assertTrue(JFiles.create(one));
 		assertTrue(one.exists() && one.isFile());
 		assertTrue(JFiles.create(nested));
 		assertTrue(nested.exists() && nested.isFile());
+		one.delete();
+		nested.delete();
+		nest.delete();
+		JTestUtils.TMP_FOLDER.delete();
 	}
 }
