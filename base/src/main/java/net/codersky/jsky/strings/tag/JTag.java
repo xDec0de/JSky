d7ea015a4b0644f434c3578146befd3099bb5351
xDec0de_ <xdec0deyt@gmail.com>
2025-05-12 22:11:04 +0200

    Added JTag#equals

--- base/src/main/java/net/codersky/jsky/strings/tag/JTag.java
+++ base/src/main/java/net/codersky/jsky/strings/tag/JTag.java
@@ -30,4 +30,23 @@ public class JTag {
 	public JTag @NotNull [] getChildren() {
 		return children;
 	}
+
+	@Override
+	public boolean equals(@Nullable Object obj) {
+		if (obj == this)
+			return true;
+		if (obj instanceof final JTag other) {
+			if (!Objects.equals(getName(), other.getName()))
+				return false;
+			if (!Objects.equals(getContent(), other.getContent()))
+				return false;
+			if (getChildren().length != other.getChildren().length)
+				return false;
+			for (int i = 0; i < getChildren().length; i++)
+				if (!getChildren()[i].equals(other.getChildren()[i]))
+					return false;
+			return true;
+		}
+		return false;
+	}
 }
