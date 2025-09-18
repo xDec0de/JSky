1711396c6c27cdfded19fb14b33075ddfde16bfa
xDec0de_ <xdec0deyt@gmail.com>
2025-09-18 23:10:25 +0200

    TestYamlFile: use AfterAll & BeforeEach to remove tmp folder

--- yaml/src/test/java/net/codersky/jsky/yaml/test/TestYamlFile.java
+++ yaml/src/test/java/net/codersky/jsky/yaml/test/TestYamlFile.java
@@ -2,6 +2,8 @@ package net.codersky.jsky.yaml.test;
 
 import net.codersky.jsky.test.JTestUtils;
 import net.codersky.jsky.yaml.YamlFile;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,8 +14,21 @@ public class TestYamlFile {
 
 	@Test
 	public void testFileCreation() {
-		JTestUtils.deleteFolder(JTestUtils.TMP_FOLDER);
 		assertTrue(testFile.setup(e -> System.err.println("Exception on YamlFile#setup: " + e.getMessage())));
 		assertTrue(testFile.asFile().exists());
+	}
+
+	/*
+	 - Before & after test actions
+	 */
+
+	@BeforeEach
+	public void beforeEach() {
+		JTestUtils.deleteFolder(JTestUtils.TMP_FOLDER);
+	}
+
+	@AfterAll
+	public static void afterAll() {
+		JTestUtils.deleteFolder(JTestUtils.TMP_FOLDER);
 	}
 }
